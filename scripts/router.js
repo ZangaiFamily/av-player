3f33b541d4d5edb54ad5bdeb19b6fe95b1b1c35a
matrixbirds <yooobuntu@gmail.com>
2019-04-15 11:19:19 +0000

    refactor. page layout

--- scripts/router.js
+++ scripts/router.js
@@ -9,4 +9,8 @@ module.exports = [
     filename: "canvas.html",
     template: path.resolve(__dirname, "../src/components/canvas/canvas.html"),
   }),
+  new HtmlWebpackPlugin({
+    filename: "camera.html",
+    template: path.resolve(__dirname, "../src/components/capture-camera/capture-camera.html"),
+  }),
 ]

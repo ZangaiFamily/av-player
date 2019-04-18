09ff78c787281d33fef406b259d72c63e22811b9
matrixbirds <yooobuntu@gmail.com>
2019-04-18 11:19:19 +0000

    refactor. multi-page

--- scripts/router.js
+++ scripts/router.js
@@ -1,16 +1,38 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-module.exports = [
+
+const map = [
+  {
+    path: 'index.html',
+    resolve: path.resolve(__dirname, "../index.html"),
+  },
+  {
+    path: 'canvas.html',
+    resolve: path.resolve(__dirname, "../src/components/canvas/canvas.html")
+  },
+  {
+    path: 'camera.html',
+    resolve: path.resolve(__dirname, "../src/components/capture-camera/capture-camera.html")
+  }
+];
+
+module.exports = map.map(({path, resolve}) => 
   new HtmlWebpackPlugin({
-    filename: "index.html",
-    template: path.resolve(__dirname, "../index.html"),
-  }),
-  new HtmlWebpackPlugin({
-    filename: "canvas.html",
-    template: path.resolve(__dirname, "../src/components/canvas/canvas.html"),
-  }),
-  new HtmlWebpackPlugin({
-    filename: "camera.html",
-    template: path.resolve(__dirname, "../src/components/capture-camera/capture-camera.html"),
-  }),
-]
+    filename: path,
+    template: resolve
+  })
+);
+// module.exports = [
+//   new HtmlWebpackPlugin({
+//     filename: "index.html",
+//     template: path.resolve(__dirname, "../index.html"),
+//   }),
+//   new HtmlWebpackPlugin({
+//     filename: "canvas.html",
+//     template: path.resolve(__dirname, "../src/components/canvas/canvas.html"),
+//   }),
+//   new HtmlWebpackPlugin({
+//     filename: "camera.html",
+//     template: path.resolve(__dirname, "../src/components/capture-camera/capture-camera.html"),
+//   }),
+// ]

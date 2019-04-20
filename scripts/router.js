d0d1ce830cf0e363f3d888e54a1ccf8fc8a0df85
matrixbirds <yooobuntu@gmail.com>
2019-04-20 11:19:19 +0000

    chore. remove comment code

--- scripts/router.js
+++ scripts/router.js
@@ -22,17 +22,3 @@ module.exports = map.map(({path, resolve}) =>
     template: resolve
   })
 );
-// module.exports = [
-//   new HtmlWebpackPlugin({
-//     filename: "index.html",
-//     template: path.resolve(__dirname, "../index.html"),
-//   }),
-//   new HtmlWebpackPlugin({
-//     filename: "canvas.html",
-//     template: path.resolve(__dirname, "../src/components/canvas/canvas.html"),
-//   }),
-//   new HtmlWebpackPlugin({
-//     filename: "camera.html",
-//     template: path.resolve(__dirname, "../src/components/capture-camera/capture-camera.html"),
-//   }),
-// ]

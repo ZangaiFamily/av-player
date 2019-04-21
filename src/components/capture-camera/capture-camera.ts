2704cc8f784da057822839cb17d8f57dc317794e
matrixbirds <yooobuntu@gmail.com>
2019-04-22 00:38:04 +0800

    feat. add media control button

--- src/components/capture-camera/capture-camera.ts
+++ src/components/capture-camera/capture-camera.ts
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", (evt: Event) => {
-  navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  }).then((stream: MediaStream) => {
-    const video: HTMLMediaElement = document.querySelector("#video")
-    video.srcObject = stream
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#open-camera").addEventListener("click", (evt: MouseEvent) => {
+    navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    }).then((stream: MediaStream) => {
+      const video: HTMLMediaElement = document.querySelector("#video")
+      video.srcObject = stream
+    })
   })
+
+  document.querySelector("#close-camera").addEventListener("click", (evt: MouseEvent) => {
+    const video: HTMLMediaElement = document.querySelector("#video")
+    const stream: MediaStream = video.srcObject as MediaStream
+    stream.getTracks().forEach((track: MediaStreamTrack) => track.stop())
+  })
+
 })

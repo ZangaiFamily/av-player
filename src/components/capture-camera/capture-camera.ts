5b2ef6b3a4813548d7dc0fd89405d0ddad79ffc4
matrixbirds <yooobuntu@gmail.com>
2019-04-22 01:00:28 +0800

    use rxjs

--- src/components/capture-camera/capture-camera.ts
+++ src/components/capture-camera/capture-camera.ts
@@ -1,5 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#open-camera").addEventListener("click", (evt: MouseEvent) => {
+import { fromEvent } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+class CaptureComponent {
+  private source: any
+  constructor() {
+    this.source = fromEvent(document, 'DOMContentLoaded')
+  }
+
+  public load(cb: (val: CustomEvent) => void) {
+    this.source.pipe(
+        map(
+          (evt: CustomEvent) => `load time: ${evt.timeStamp}`),
+      )
+      .subscribe(cb)
+  }
+
+  public observers(id: string, cb: (val: CustomEvent) => void) {
+    const source = fromEvent(document.querySelector(`#${id}`), "click")
+    const producer = source.pipe(
+      map(
+        (evt: MouseEvent) => `#${id} Event time: ${evt.timeStamp}`),
+    ).subscribe(cb)
+  }
+}
+
+const comp = new CaptureComponent()
+
+comp.load((_: CustomEvent) => {
+  comp.observers("open-camera", (val: CustomEvent) => {
     navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
@@ -7,12 +35,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const video: HTMLMediaElement = document.querySelector("#video")
       video.srcObject = stream
     })
+    console.log(val)
   })
 
-  document.querySelector("#close-camera").addEventListener("click", (evt: MouseEvent) => {
+  comp.observers("close-camera", (val: CustomEvent) => {
     const video: HTMLMediaElement = document.querySelector("#video")
     const stream: MediaStream = video.srcObject as MediaStream
     stream.getTracks().forEach((track: MediaStreamTrack) => track.stop())
+    console.log(val)
   })
-
 })
